84b771faa2b08aea8b7b6f484de891218dd748d3
AnaBelen <anahernandezfit@gmail.com>
2024-09-23 14:57:48 +0200

    Creación de rama feature/develop

--- adivinarJuego.java
+++ adivinarJuego.java
@@ -1,3 +1,12 @@
-public class adivinarJuego {
+public class adivinarJuego { 
+        //Crear una rama nueva   
+        //Declarar las variables y generar el número aleatorio de 0 a 100
+        // Establecer reinicio
+        // Número de intentos = a 0 de inicio.
+        // Ingresar números
+        // Si acierta el número, mensaje de que se acaba el juego, sino no acierta el número, reinicio o continuar
+        // Si meto un número negativo o mayor de 100, mensaje de error y recordar qué es del 0 al 100
+        public static void main(String[] args) {
+            }
     
 }
