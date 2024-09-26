dd16595418c8d8ef7d976326debe4e4bba2a4e78
AnaBelen <anahernandezfit@gmail.com>
2024-09-26 15:03:22 +0200

    game conecta finally finished!

--- conecta.java
+++ conecta.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class conecta {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -6,16 +7,27 @@ public class conecta {
         // Declarar variables
         int filas = 6;
         int columnas = 7;
+        char symbol = 'X';
         char[][] table = new char[filas][columnas];
+
         int valueInput;
         int count = 1;
 
         // True => Player1, False => Player2
         boolean turnPlayer = true;
         boolean win = false;
+
         System.out.println();
         System.out.println("Bienvenido al Juego de CONECTA 4!");
 
+        // Inicializamos tablero con casillas vacías
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                table[i][j] = ' ';
+            }
+        }
+
+        // Bucle siempre que WIN no sea true
         do {
             System.out.println();
             System.out.println("Jugador 1 = X || Jugador 2 = O");
@@ -34,16 +46,44 @@ public class conecta {
             System.out.println("MENÚ - Seleccione una columna del 1 al 7 (de izq a dcha)");
             valueInput = scanner.nextInt() - 1;
 
-            // AQUÍ VA LA PARTE DE INCLUIR FICHA
-            // addMove(table, columnas, turnPlayer);
+            // Llamamos método para incluir ficha del jugador
+            if (addMove(table, columnas, turnPlayer, valueInput)) {
+                symbol = turnPlayer ? 'X' : 'O';
 
-            turnPlayer = !turnPlayer;
-            count++;
+                // Comprobar si hay X | O repetido 4 veces en vertial u horizontal
+                if (checkWinner(table, columnas, filas, symbol)) {
+                    win = true;
+                }
+
+                turnPlayer = !turnPlayer;
+                count++;
+            }
         } while (!win);
+
+        if (win) {
+            if (turnPlayer) {
+                System.out.println();
+                System.out.println("Enhorabuena! Ha ganado el Jugador 1 en " + count + " movimientos");
+                System.out.println();
+
+                printTable(table);
+
+                System.out.println();
+            } else {
+                System.out.println();
+                System.out.println("FINAL!! Enhorabuena! Ha ganado el Jugador 2 en " + count + " movimientos");
+                System.out.println();
+
+                printTable(table);
+
+                System.out.println();
+            }
+        }
 
         scanner.close();
     }
 
+    // Dibujo de tabla
     public static void printTable(char[][] table) {
         for (int i = 0; i < table.length; i++) {
             for (int j = 0; j < table[i].length; j++) {
@@ -54,23 +94,79 @@ public class conecta {
         }
     }
 
-    public static boolean addMove(char[][] table, int columnas, boolean turnPlayer) {
+    // Añadir ficha-movimiento del jugador
+    public static boolean addMove(char[][] table, int columnas, boolean turnPlayer, int valueInput) {
         boolean move = false;
+
+        // Condición valor > 0 || valor fuera de rango columnas
+        if (valueInput < 0 || valueInput >= columnas) {
+            System.out.println("Movimiento inválido! El número de columna está fuera del rango");
+            return move;
+        }
 
         // Buscamos desde la última fila
         for (int i = table.length - 1; i >= 0; i--) {
-            if (table[i][columnas] == ' ') {
+            if (table[i][valueInput] == ' ') {
                 if (turnPlayer) {
-                    table[i][columnas] = 'X';
+                    table[i][valueInput] = 'X';
                 } else {
-                    table[i][columnas] = 'O';
+                    table[i][valueInput] = 'O';
                 }
-
                 move = true;
                 break;
             }
         }
-        
+
+        // Debemos comprobar que la columna no está llena
+        if (!move) {
+            System.out.println("La columna está llena! Debe insertar ficha en otra distinta");
+        }
+
         return move;
+    }
+
+    // Comprobación de winner por columnas, filas y diagonal
+    public static boolean checkWinner(char[][] table, int columnas, int filas, char symbol) {
+        // Horizontal
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length - 3; j++) {
+                if (table[i][j] == symbol && table[i][j + 1] == symbol && table[i][j + 2] == symbol
+                        && table[i][j + 3] == symbol) {
+                    return true;
+                }
+            }
+        }
+
+        // Vertical
+        for (int i = 0; i < table.length - 3; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                if (table[i][j] == symbol && table[i + 1][j] == symbol && table[i + 2][j] == symbol
+                        && table[i + 3][j] == symbol) {
+                    return true;
+                }
+            }
+        }
+
+        // Diagonal de izquierda a derecha
+        for (int i = 0; i < table.length - 3; i++) {
+            for (int j = 0; j < table[i].length - 3; j++) {
+                if (table[i][j] == symbol && table[i + 1][j + 1] == symbol && table[i + 2][j + 2] == symbol
+                        && table[i + 3][j + 3] == symbol) {
+                    return true;
+                }
+            }
+        }
+
+        // Diagonal de derecha a izquierda
+        for (int i = 3; i < table.length; i++) {
+            for (int j = 0; j < table[i].length - 3; j++) {
+                if (table[i][j] == symbol && table[i - 1][j + 1] == symbol && table[i - 2][j + 2] == symbol
+                        && table[i - 3][j + 3] == symbol) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
     }
 }
